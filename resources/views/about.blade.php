e5990e4b76511df2542e88c5bbf2d2e1c89da4e0
Chizzo Cheese <chizzo84@gmail.com>
2019-12-22 04:56:54 +0200

    Update about.blade.php
    Added Google maps location

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,5 +5,9 @@
         <main class="py-4">
             <p>Makeni Assembly was born on 17th April, 1994. The vision was simply to take Christ to the community and the community to Christ.</p>
         </main>
+        <center>
+            <h3>MAOG Church Location</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3845.124202775005!2d28.24585761485105!3d-15.477749989248675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1940f25eb459e44f%3A0xebf5a30f2acbbfa4!2sMakeni%20Assembly%20of%20God%20Church!5e0!3m2!1sen!2szm!4v1576983091607!5m2!1sen!2szm" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        </center>
     </div>
 @endsection
