83c9ae89c176e203a472888914322b0a8cdd4f42
Chizzo Cheese <chizzo84@gmail.com>
2019-12-22 22:25:30 +0200

    Correct image referencing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
-    <link rel='manifest' href='/manifest.webmanifest'>
+    <link rel='manifest' href="{{ asset('manifest.webmanifest') }}">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
