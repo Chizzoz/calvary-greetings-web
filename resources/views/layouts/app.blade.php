bb2a1f049fa0d476e283a67f6fd81372ff35e171
Chizzo Cheese <chizzo84@gmail.com>
2019-12-22 22:14:45 +0200

    Correct manifest conflict and wrong asset links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -191,7 +191,7 @@
     </footer>
 
     <!-- Scripts -->
-    <script async type="text/javascript" src="../js/burger.js"></script>
+    <script async type="text/javascript" src="{{ asset('js/burger.js') }}"></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/overlayscrollbars/1.9.1/js/OverlayScrollbars.min.js'></script>  
     <script>
     document.addEventListener("DOMContentLoaded", function() {
